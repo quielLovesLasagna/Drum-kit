315470a74ff9ff782f4ac47abd125eaae33d83d7
Exequiel Arco <exequielarco23@gmail.com>
2023-10-17 23:00:10 +0800

    Refactored Everything

--- index.js
+++ index.js
@@ -1,81 +1,80 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+"use strict";
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    
-    var buttonInnerHTML = this.innerHTML;
+// Elements //
+const drumContainer = document.querySelector(".set");
+// End of Elements //
 
-    makeSound(buttonInnerHTML);
-
-    buttonAnimation(buttonInnerHTML);
-
-  });
-
-}
-
-
-document.addEventListener("keydown", function(event) {
-
-  makeSound(event.key);
-
-  buttonAnimation(event.key);
-
-});
-
-
-function makeSound(key) {
-
-  switch (key) {
+// Functions //
+const makeSound = (e) => {
+  switch (e) {
     case "w":
-        var tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
+      const tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
       break;
 
     case "a":
-        var tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
+      const tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
       break;
-    
+
     case "s":
-        var tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
+      const tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
       break;
 
     case "d":
-        var tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
+      const tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
       break;
 
     case "j":
-        var snare = new Audio("./sounds/snare.mp3");
-        snare.play();
+      const snare = new Audio("./sounds/snare.mp3");
+      snare.play();
       break;
 
     case "k":
-        var crash = new Audio("./sounds/crash.mp3");
-        crash.play();
+      const crash = new Audio("./sounds/crash.mp3");
+      crash.play();
       break;
 
     case "l":
-        var kick = new Audio("./sounds/kick-bass.mp3");
-        kick.play();
+      const kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
       break;
-    
-  
+
     default:
-        console.log(buttonInnerHTML);
-      break;
   }
+};
 
-}
-
-
-function buttonAnimation(currentKey) {
-
-  var activeButton = document.querySelector("." + currentKey);
-
+const drumAnimation = (e) => {
+  const keyToClass = {
+    w: "w",
+    a: "a",
+    s: "s",
+    d: "d",
+    j: "j",
+    k: "k",
+    l: "l",
+  };
+  const className = keyToClass[e];
+  if (!className) return;
+  const activeButton = document.querySelector(`.${e}`);
   activeButton.classList.add("pressed");
+  setTimeout(() => activeButton.classList.remove("pressed"), 100);
+};
 
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
-  }, 100);
-}
+// End of Functions //
+
+// Event Handlers //
+drumContainer.addEventListener("click", (e) => {
+  const btnInnerHTML = e.target.innerHTML;
+  makeSound(btnInnerHTML);
+  drumAnimation(btnInnerHTML);
+});
+
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+  makeSound(key);
+  drumAnimation(key);
+});
+// End of Event Handlers //
